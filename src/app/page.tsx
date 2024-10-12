789ecced5050ce6487357edeb46fa4d470ed74a2
@MaikonRodrigs <maikonrodrigss@gmail.com>
2024-10-12 02:34:01 -0300

    config: branch and repositorie

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '@/pages/dashboard'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Dashboard />
+    </div>
+  )
+}
